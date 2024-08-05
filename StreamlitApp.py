7cf7cdc4cd85da45dd7bbbbd18e491269d719f51
devanshs288 <devansh>
2024-08-05 15:20:43 +0530

    changed directory name

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from langchain_community.callbacks import get_openai_callback
-from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.mcqgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
 # Loading json file 
